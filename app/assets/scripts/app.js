40790f6c56af831300b484154dee846558a75a96
mesh6726 <mesh6726@gmail.com>
2018-08-20 03:49:12 +0800

    ??

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
-import Modal from './modules/modal';
-
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-var modal = new Modal();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
